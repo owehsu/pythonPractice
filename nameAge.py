85062812cef338752c980b5c0e9a6ec0b2e6bd6f
owehsu <49104763+owehsu@users.noreply.github.com>
2019-03-30 23:32:55 -0700

    Name Age function practice

---
+++ nameAge.py
@@ -0,0 +1,32 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> def nameAge():
+	name = input('What is your name? ')
+	age = int(input('What is your age? '))
+	number = int(input('Choose a number: '))
+	old = str(2019 + (100-age))
+	for i in range(number(:
+			      
+SyntaxError: invalid syntax
+>>> def nameAge():
+	name = input('What is your name? ')
+	age = int(input('What is your age? '))
+	number = int(input('Choose a number: '))
+	old = str(2019 + (100-age))
+	for i in range(number):
+		print('Hi ' + name + ', in the year ' + old + ', you will be 100 years old!\n')
+
+			      
+>>> nameAge()
+			      
+What is your name? Owen
+What is your age? 14
+Choose a number: 3
+Hi Owen, in the year 2105, you will be 100 years old!
+
+Hi Owen, in the year 2105, you will be 100 years old!
+
+Hi Owen, in the year 2105, you will be 100 years old!
+
+>>> 
